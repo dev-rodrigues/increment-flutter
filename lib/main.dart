6bc62f5305ef3f95694d660f6ba8fb5fa641fa45
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2021-07-10 14:57:31 -0300

    aplicacao contador

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.deepOrange), home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Counter"),
+      ),
+      body: Center(
+        child: Center(
+            child: Text(
+          "Counter\n$count",
+          textAlign: TextAlign.center,
+        )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          increment();
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
